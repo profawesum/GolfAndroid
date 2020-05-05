6653787cdd70ac37336e3fd125a4d64d52788acd
Harry <profawesum@gmail.com>
2020-05-05 16:41:59 +1200

    Pushing the game

---
+++ AndroidGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "KillFloor")
+        {
+            //restart the level
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        if (other.tag == "LevelEnd") {
+            //load into level select screen
+            Application.LoadLevel("LevelSelect");
+        }
+    }
+}
